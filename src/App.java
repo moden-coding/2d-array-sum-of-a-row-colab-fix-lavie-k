2abc02eb5a8705a87644d098c85dd3a3a0ca8b7c
Nadav Konstantine <nadavk2002@gmail.com>
2026-01-16 12:47:11 -0500

    2

--- src/App.java
+++ src/App.java
@@ -9,7 +9,11 @@ public class App {
 		System.out.println(sumRow(a,0)); //should print 17
 	}
 	public static int sumRow(int[][] array, int row){
-		return -1;
+		int sum = 0;
+		for(int i = 0; i < array[row].length; i++){
+			sum += array[row][i];
+		}
+		return sum;
 	}
 	
 }
